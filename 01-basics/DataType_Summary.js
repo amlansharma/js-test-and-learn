d6c41fda539362e05954ae07a825d8679adf6ee0
Amlan Sharma <77141364+amlansharma@users.noreply.github.com>
2023-12-04 17:56:36 +0000

    DataTypeSummary added

---
+++ 01-basics/DataType_Summary.js
@@ -0,0 +1,13 @@
+/*1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint const bI = 12312312312312321n
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  object-function
+       Object  =>  object*/
