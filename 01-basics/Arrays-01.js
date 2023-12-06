c7bfdf5018df8290ce9341de44a96d6a70abca8b
Amlan Sharma <77141364+amlansharma@users.noreply.github.com>
2023-12-06 05:55:29 +0000

    arrays

---
+++ 01-basics/Arrays-01.js
@@ -0,0 +1,18 @@
+let myArray = [0,1,2,3,4]
+;
+//console.log(typeof myArray);
+
+//let mySecondArray = new Array(1,5,6)
+//console.log(mySecondArray);
+
+// myArray.push(5)
+// myArray.push(6,7,8)
+// console.log(myArray)
+// myArray.pop()
+// console.log(myArray)
+
+//myArray.unshift(10)
+console.log(myArray)
+
+myArray.shift()
+console.log(myArray)
